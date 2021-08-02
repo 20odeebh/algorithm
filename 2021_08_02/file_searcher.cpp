a64a56cf41a704b91245ce12ca44900df39a875c
jessi68 <lynn312.unist.ac.kr>
2021-08-02 23:22:11 +0900

    가희 문자열 성공

--- 2021_08_02/file_searcher.cpp
+++ 2021_08_02/file_searcher.cpp
@@ -10,31 +10,34 @@ struct file {
     string name;
     string extension;
     bool perceivedByOs;
-    file(string _name, string _extension) : name(_name), extension(_extension){
+    file(string _name, string _extension, bool _perceivedbyOs) : name(_name), extension(_extension), perceivedByOs(_perceivedbyOs){
         
     };
 };
 
-vector<file> files;
 
 
-bool compare(const file & a, const file & b) {
-    if(a.name.compare(b.name) == 0) {
-        if(a.perceivedByOs && b.perceivedByOs) {
-            return a.extension.compare(b.extension) < 0;
-        } else if(a.perceivedByOs) {
+bool compare(const file * a, const file * b) {
+     
+    if(a->name == b->name) {
+        
+        if(a->perceivedByOs && b->perceivedByOs) {
+            return a->extension < b->extension;
+        } else if(a->perceivedByOs) {
             return true;
-        } else if(b.perceivedByOs) {
+        } else if(b->perceivedByOs) {
             return false;
         } else{
-            a.extension.compare(b.extension) < 0;
+            return a->extension < b->extension;
         }
     } else{
-        return a.name.compare(b.name) < 0;
+        return a->name < b->name;
     }
+
 }
 
 int main() {
+    vector<file*> files;
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
@@ -42,16 +45,16 @@ int main() {
     int extensionNum;
     unordered_map<string, bool> extensionToPerceivedStates;
     cin >> fileNum >> extensionNum;
-
+    file * f;
     for(int i = 0; i < fileNum; i++) {
         string temp;
         cin >> temp;
         int index = temp.find(".");
         string name = temp.substr(0, index);
         string extension = temp.substr(index + 1);
-        file * f = new file(name, extension);
+        f = new file(name, extension, false);
         f->perceivedByOs = false;
-        files.push_back(*f);
+        files.push_back(f);
         
         extensionToPerceivedStates[extension] = false;
     }
@@ -59,24 +62,23 @@ int main() {
     for(int i = 0; i < extensionNum; i++) {
         string extension;
         cin >> extension;
-       
         extensionToPerceivedStates[extension] = true;
       
     }
 
     for(auto file: files)  {
-        file.perceivedByOs = extensionToPerceivedStates[file.extension];
+        
+        file->perceivedByOs = extensionToPerceivedStates[file->extension];
+        
     }
     
     sort(files.begin(), files.end(), compare);
 
-    for(vector<file>::iterator iter = files.begin(); iter != files.end(); iter++) {
-        cout << (*iter).name + "." + (*iter).extension << "\n";
+    for(vector<file*>::iterator iter = files.begin(); iter != files.end(); iter++) {
+        cout << (*iter)->name + "." + (*iter)->extension << "\n";
     }
     
 
 }
-
-
 
 
